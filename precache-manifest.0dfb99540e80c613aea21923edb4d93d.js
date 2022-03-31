32b584742f0105942ef0ebb809c736fa0d941b44
Orest Sharak <sharak.orest@gmail.com>
2022-03-31 11:36:56 +0200

    Updates

---
+++ precache-manifest.0dfb99540e80c613aea21923edb4d93d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a430384769b3740d55d75d8c8e6ebe",
+    "url": "/react-redux-auth/index.html"
+  },
+  {
+    "revision": "da48be44bc5061177cfd",
+    "url": "/react-redux-auth/static/css/2.f8731e4e.chunk.css"
+  },
+  {
+    "revision": "74037ff24f9e9416b2a2",
+    "url": "/react-redux-auth/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "da48be44bc5061177cfd",
+    "url": "/react-redux-auth/static/js/2.e4a1560a.chunk.js"
+  },
+  {
+    "revision": "1e12d9305fc0881e8f231c6708eb8b91",
+    "url": "/react-redux-auth/static/js/2.e4a1560a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74037ff24f9e9416b2a2",
+    "url": "/react-redux-auth/static/js/main.f21bc91e.chunk.js"
+  },
+  {
+    "revision": "389114bf74f75496c544",
+    "url": "/react-redux-auth/static/js/runtime-main.f850d6da.js"
+  }
+]);
